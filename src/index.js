6e4140e89ee090375b4bd9cb198fb5b1784fb297
arizh <arish0246@gmail.com>
2019-07-19 23:57:42 +0530

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App.jsx';
 import './styles/main.scss';
+import reducer from './store/reducers/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+}
+
+export default reducer;
