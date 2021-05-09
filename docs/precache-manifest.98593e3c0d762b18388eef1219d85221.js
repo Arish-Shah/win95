79b497e6f7240d867cb5f6d671e559c151a4f1ba
Arish-Shah <arish0246@gmail.com>
2021-05-09 12:14:26 +0530

    update homepage

---
+++ docs/precache-manifest.98593e3c0d762b18388eef1219d85221.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6944564995290a59a8d3da9338a0cea2",
+    "url": "/win95/index.html"
+  },
+  {
+    "revision": "fdc842faed1e11d28248",
+    "url": "/win95/static/css/main.7db232a7.chunk.css"
+  },
+  {
+    "revision": "b3f0f0671b7430a9fa3b",
+    "url": "/win95/static/js/2.144231db.chunk.js"
+  },
+  {
+    "revision": "fdc842faed1e11d28248",
+    "url": "/win95/static/js/main.c4a55c87.chunk.js"
+  },
+  {
+    "revision": "e1a16012ed3795a778ac",
+    "url": "/win95/static/js/runtime~main.07cbb5a3.js"
+  },
+  {
+    "revision": "bf40df6254d9fa37eca8cbb28635b506",
+    "url": "/win95/static/media/Fixedsys.bf40df62.woff2"
+  },
+  {
+    "revision": "faea3367f031e3c407a88316ccc93c45",
+    "url": "/win95/static/media/MSSansSerif.faea3367.woff2"
+  },
+  {
+    "revision": "5eea62875443d3f0996fde5929fae727",
+    "url": "/win95/static/media/MSSansSerifBold.5eea6287.woff2"
+  },
+  {
+    "revision": "6863793f81edf99f1d8f6d062d172f56",
+    "url": "/win95/static/media/shutdown.6863793f.png"
+  },
+  {
+    "revision": "337a0b0ec9de91fcece69894f54bf610",
+    "url": "/win95/static/media/startup.337a0b0e.mp3"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c211088f6d45f16c984f65aa3e2b672.js"
+  "/win95/precache-manifest.98593e3c0d762b18388eef1219d85221.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/win95/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
